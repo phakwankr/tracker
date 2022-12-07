f51ace31ecf0c8f7e339c59bf0caec0a7dc5ef79
Phakwan Kruerattanakul <ph_kwankr@outlook.com>
2022-12-07 15:50:55 +0700

    Third Edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,31 +6,34 @@ import {
   Typography, 
   Button, 
   Tabs, 
-  Tab 
+  Tab, 
 } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  
   const [value, setValue] = useState();
   return (
     <AppBar 
-    position="stick"
+    position="sticky"
       sx={{ 
         background:
           'linear-gradient(90deg, rgba(166,135,187,1) 0%, rgba(255,190,190,1) 50%, rgba(241,207,160,1) 100%);' 
       }}
     >
       <Toolbar>
-        <Typography variant='h5'>Up 'n Down</Typography>
-        <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
-          <Tabs 
-            textColor='inherit' 
-            value={value} 
-            onChange={(e,val) => setValue(val)}
-          >
-            <Tab LinkComponent={Link} to="/home" label="Home"/>
-            <Tab LinkComponent={Link} to="/myDashboard" label="My Dashboard"/>
-          </Tabs>
-        </Box>
+        <Typography variant="h5">Up 'n Down</Typography>
+     { isLoggedIn &&  <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
+        <Tabs
+          textColor="inherit"
+          value={value}
+          onChange={(e, val) => setValue(val)}
+        >
+          <Tab LinkComponent={Link} to="/home" label="Home" />
+          <Tab LinkComponent={Link} to="/myDashboard" label="My Dashboard" />
+        </Tabs>
+        </Box>}
         <Box display="flex" marginLeft="auto">
           <Button 
             LinkComponent={Link} 
@@ -48,13 +51,15 @@ const Header = () => {
             color="warning">Sign Up</Button
           >
 
-          <Button 
-            LinkComponent={Link} to="/auth"
+        { isLoggedIn && <Button 
+            LinkComponent={Link} 
+            to="/auth"
             variant="contained" 
             sx={{ margin:'1',borderRadius:10 }} 
-            color="warning">Log Out</Button
-          >
-
+            color="warning"
+            >
+            Log Out
+            </Button>}
         </Box>
       </Toolbar>
     </AppBar>
